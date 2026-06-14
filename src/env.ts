71e38e65b9dcbf7782f20737d6928c196413382a
Oscar Perez <oaperezsanchez2925@gmail.com>
2026-06-14 12:05:41 -0500

    add initial configuration and components next theme

---
+++ src/env.ts
@@ -0,0 +1,17 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import z from "zod";
+
+export const env = createEnv({
+    server: {
+        DATABASE_URL: z.url(),
+        UPLOADTHING_TOKEN: z.string(),
+
+        BETTER_AUTH_SECRET: z.string(),
+    },
+    client: {
+        NEXT_PUBLIC_BETTER_AUTH_URL: z.url(),
+    },
+    experimental__runtimeEnv: {
+        NEXT_PUBLIC_BETTER_AUTH_URL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL,
+    },
+});
